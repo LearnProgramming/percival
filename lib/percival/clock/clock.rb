64d75c4f2f13e07af2ef7fccc5dcf0b78350fe19
Joe Fredette <jfredett@gmail.com>
2011-12-02 08:42:02 -0500

    set up some scaffolding for the acceptance tests

--- lib/percival/clock/clock.rb
+++ lib/percival/clock/clock.rb
@@ -3,4 +3,17 @@ class Clock
   def self.reset!(user_name)
 
   end
+
+  def self.for(user_name)
+    []
+  end
+
+  def self.in
+
+  end
+
+  def self.out
+
+  end
+
 end
--- spec/clock_spec.rb
+++ spec/clock_spec.rb
@@ -9,22 +9,60 @@ describe "Clock class methods" do
     end
   end
 
+  describe ".for(user)" do
+    it "takes a single user as an argument" do
+      expect { subject.for("foo") }.should_not raise_error
+    end
+  end
+
+  describe ".in" do
+    it "takes no arguments" do
+      expect { subject.in }.should_not raise_error
+    end
+  end
+
+  describe ".out" do
+    it "takes no arguments" do
+      expect { subject.out }.should_not raise_error
+    end
+  end
 end
 
 
 describe "Using the !clock command to clock in and out" do
   before { Clock.reset!("test_user") }
+  let(:channel) do 
+    IrcFaker.new(cinch_mock)
+  end
+
+  let(:cinch_mock) do
+    mock(:cinch).tap do |m|
+      m.stub!(:reply)
+    end
+  end
+  
 
   it "allows me to clock in by sending the message `!clock in` to the channel" do
-    channel.send_message("!clock in").as("test_user")
-    Clock.for("test_user").should =~ [Clock.in("test_user")]
+    channel.
+      send_message("!clock in").
+      as("test_user").
+    done
+
+    Clock.for("test_user").should =~ [Clock.in]
   end
 
   it "allows me to clock out by sending the message `!clock out` to the channel" do
-    channel.send_message("!clock in").as("test_user")
-    Clock.for("test_user").should =~ [Clock.in("test_user")]
-    channel.send_message("!clock out").as("test_user")
-    Clock.for("test_user").should =~ [Clock.in("test_user")]
+    channel.
+      send_message("!clock in").
+      as("test_user").
+    done
+    Clock.for("test_user").should =~ [Clock.in]
+
+    channel.
+      send_message("!clock out").
+      as("test_user").
+    done
+    Clock.for("test_user").should =~ [Clock.in, Clock.out]
   end
 
   pending "design" do
