aa6cf22e36685e77bf18a9059a4c4f8e639f6503
Joe Fredette <jfredett@gmail.com>
2011-12-07 11:24:05 -0500

    trying to clock in twice with the same type now raises an error

--- lib/percival/clock/timesheet.rb
+++ lib/percival/clock/timesheet.rb
@@ -1,11 +1,13 @@
 class Timesheet
   def self.entry(user, type, reader=File)
     raise "Reader dependency must respond to #open" unless reader.respond_to? :open
+    raise "Reader dependency must respond to #read" unless reader.respond_to? :read
 
     new(user, reader).clock(type)
   end
 
   def clock(type)
+    raise InvalidTimesheetSequence if type == previous_type
     write_to_timesheet(Tick.send(type).dump)
   end
 
@@ -19,10 +21,37 @@ class Timesheet
   end
 
   def write_to_timesheet(string)
-    reader.open(timesheet_path, 'w') do |f|
+    reader.open(timesheet_path, 'a') do |f|
       f << string
+      f << '\n'
     end
     self
+  end
+
+  def timesheet_data 
+    raw_data = reader.read(timesheet_path)
+    return [] unless raw_data
+    
+    entries = []
+    entry = ""
+    raw_data.each_line do |line|
+      if line =~ /^\s*$/
+        entries << YAML.load(entry)
+        entry = "" 
+      else
+        entry << line
+      end
+    end
+    entries << YAML.load(entry) #catches the last entry on the list (one which is not trailed by an empty line)
+
+    entries
+  end
+
+  def previous_type
+    last_entry = (timesheet_data || []).sort.last
+    #another minor hack -- I want a "only other instances of myself need to
+    #access this for comparison purposes" protection level.
+    last_entry && last_entry.send(:type) 
   end
 
   def timesheet_path
@@ -33,3 +62,5 @@ class Timesheet
     "/data/timesheets/" 
   end
 end
+
+class InvalidTimesheetSequence < Exception ; end
--- spec/timesheet_spec.rb
+++ spec/timesheet_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Timesheet do #class methods
   subject { Timesheet } 
   
-  let(:reader) { mock("reader").tap { |r| r.stub!(:open) } }
+  let(:reader) { mock("reader").tap { |r| r.stub!(:open) ; r.stub!(:read) } }
   let(:invalid_reader) { mock("invalid reader") }
 
   it { should respond_to :entry }
@@ -13,19 +13,42 @@ describe Timesheet do #class methods
       expect { subject.entry('test_user', :out, reader) }.should_not raise_error
     end
 
-    it "optionally expects a dependency which responds to #open" do
+    it "optionally expects a dependency which responds to #open and #read" do
       expect { subject.entry('test', :in, reader) }.should_not raise_error
       expect { subject.entry('test', :in, invalid_reader) }.should raise_error
+    end
+
+    it "should throw an error if you try to make the same entry type twice in a row" do
+      fake_initial_data =<<-HERE
+--- !ruby/object:Tick
+time: 2011-12-06 08:19:22.174495000 -05:00
+type: :out
+      HERE
+      fake_data_after_clockin =<<-HERE
+--- !ruby/object:Tick
+time: 2011-12-06 08:19:22.174495000 -05:00
+type: :out
+
+--- !ruby/object:Tick
+time: 2011-12-06 08:23:00.546090000 -05:00
+type: :in
+      HERE
+
+      expect do
+        reader.should_receive(:read).with(anything).and_return(fake_data_after_clockin)
+        reader.stub(:read).and_return(fake_data_after_clockin)
+
+        subject.entry('test', :in, reader)
+      end.should raise_error InvalidTimesheetSequence
     end
   end
 
   describe "#clock" do
     it "should write the tick to the user's timesheet" do
-      reader.should_receive(:open).with(PERCIVAL_ROOT + '/data/timesheets/test', 'w')
+      reader.should_receive(:open).with(PERCIVAL_ROOT + '/data/timesheets/test', 'a')
 
-      subject.entry('test', :in, reader)
+      subject.entry('test', :in, reader) # this feels a little wrong, it calls clock internally
     end
   end
-  
 end
 
