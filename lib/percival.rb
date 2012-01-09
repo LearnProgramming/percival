2f742030c7ac0be4fc9ffff07f2f83a96cc52aa2
Joe Fredette <jfredett@gmail.com>
2012-01-09 08:29:42 -0500

    set up logger scaffolding

--- lib/percival.rb
+++ lib/percival.rb
@@ -2,6 +2,7 @@ require 'cinch'
 
 require 'percival/version'
 require 'percival/clock'
+require 'percival/logger'
 
 PERCIVAL_ROOT = File.dirname(File.dirname(__FILE__))
 
---
+++ spec/logger_spec.rb
@@ -0,0 +1,4 @@
+describe Logger do
+
+end
+
