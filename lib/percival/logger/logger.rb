021b9aebb62ded2f348dc07e769cacf0a9c98a8c
Joe Fredette <jfredett@gmail.com>
2012-05-11 11:51:38 -0400

    fix logger plugin

--- lib/percival/logger/logger.rb
+++ lib/percival/logger/logger.rb
@@ -1,11 +1,27 @@
 require 'singleton'
+require 'forwardable'
 class Logger
+  extend Forwardable
+
   def initialize
-    @file = File.open(PERCIVAL_ROOT + '/data/log', 'a')
+    #since we're keeping a persistent connection, we need
+    #to remember to close the file when the object dies.
+    ObjectSpace.define_finalizer(self, proc { flush ; close })
   end
 
-  def log(user, message)
-    @file << "#{user}: #{message}\n"
-    @file.flush
+  delegate [:flush, :close] => :file
+
+  def log(user, message, event)
+    file << "%s | %8s | %s: %s\n" % [iso_standard_time, event, user, message]
+  end
+
+  private 
+
+  def file
+    @file ||= File.open(PERCIVAL_ROOT + '/data/log', 'a')
+  end
+
+  def iso_standard_time
+    Time.now.strftime("%F %T")
   end
 end
