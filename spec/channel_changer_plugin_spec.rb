66034c054e7b4b3cbff3d239ebfc4fd968b67c28
Martin Colwell <colwem@gmail.com>
2012-07-22 16:12:42 -0400

    added spec for channel changer

---
+++ spec/channel_changer_plugin_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "Channel Changer plugin" do
+  describe "Leaving channels" do
+    it "should leave a channel when !leave-channel is called by a privilaged user" do
+      
+    end
+    it "should not leave a channel when !leave-channel is sent by an un-privilaged user" do
+      
+    end
+  end
+
+  describe "Joining channels" do
+    it "should join channel when !join-channel [channel] is sent by privilaged user" do
+
+    end
+    it "should not join channel when !join-channel [channel] is sent by un-privilaged user" do
+
+    end
+  end
+end
