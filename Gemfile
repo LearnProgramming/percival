34789c960f3122e8d5dee62b2fe3f6be3a268211
SirSkidmore <taylor@taylorskidmore.com>
2013-01-06 14:27:17 -0600

    Moving bot out of Rakefile/rm Gemfile

--- Gemfile
+++
@@ -1,7 +0,0 @@
-source 'http://rubygems.org'
-
-gem 'rspec'
-gem 'pry'
-
-# Specify your gem's dependencies in percival.gemspec
-gemspec
--- lib/percival.rb
+++ lib/percival.rb
@@ -6,6 +6,7 @@ require 'percival/logger'
 require 'percival/channel_changer'
 require 'percival/name_changer'
 require 'percival/quote'
+require 'percival/guessing_game'
 
 PERCIVAL_ROOT = File.dirname(File.dirname(__FILE__))
 
